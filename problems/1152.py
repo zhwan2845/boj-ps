060f12fb609368bec9eb59de1d9d23635cdf96b5
zhwan2845 <108276100+zhwan2845@users.noreply.github.com>
2022-08-01 22:36:45 +0900

    Update 1152.py

--- problems/1152.py
+++ problems/1152.py
@@ -1,12 +1,33 @@
-t = input()
-if len(t) > 1000000:
-    print("잘못했습니다.")
-else:
-    words = t.split(' ')
-    cnt = 0
+# t = input()
+# if len(t) > 1000000:
+#     print("잘못했습니다.")
+# else:
+#     words = t.split(' ')
+#     cnt = 0
 
-    for i in words:
-        if i.isalpha():
-            cnt += 1
+#     for i in words:
+#         if i.isalpha():
+#             cnt += 1
 
-print(cnt)
+# print(cnt)t = input()
+# if len(t) > 1000000:
+#     print("잘못했습니다.")
+# else:
+#     words = t.split(' ')
+#     cnt = 0
+
+#     for i in words:
+#         if i.isalpha():
+#             cnt += 1
+# print(cnt)
+
+# s = input()
+# cnt = 1
+# for i in range(len(s)):
+#     if " " in s[i]:
+#         cnt += 1
+
+# print(cnt)
+
+s = input().split()
+print(len(s))    
